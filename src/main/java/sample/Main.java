d1d8141762f62fa92418c51d9b0e1ea62ea9b971
mikhailshepelev <romanist-mike@yandex.ru>
2020-03-07 19:23:00 +0200

    second commit

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
     }
 
