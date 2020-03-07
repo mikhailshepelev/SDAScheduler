d3cc49c8f28376bab150b6b93435e64b3132dfd4
mikhailshepelev <romanist-mike@yandex.ru>
2020-03-07 12:55:47 +0200

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Triin");
-        System.out.println("make changes");
+        System.out.println("make changesfsdfs");
     }
 }
